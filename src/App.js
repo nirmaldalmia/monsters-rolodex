65e5f57fd4018a8431f3a9c5ccdd846e2c240d8c
nirmaldalmia <nirmaldalmia17@gmail.com>
2019-07-26 16:59:28 +0530

    finished basic cards

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "a1"
-        },
-        {
-          name: "Dracula",
-          id: "a2"
-        },
-        {
-          name: "Zombie",
-          id: "a3"
-        }
-      ]
+      monsters: []
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
