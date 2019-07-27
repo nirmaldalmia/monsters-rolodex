ebc59f6a0e3857aeb61b83936280927fe32ea246
nirmaldalmia <nirmaldalmia17@gmail.com>
2019-07-27 13:27:01 +0530

    Updates

---
+++ precache-manifest.de6f05b2651536eaf3b80e467e777910.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a497344084a4ff097f79dfd61c33836",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a13d69c5ca2cb013e61c",
+    "url": "/monsters-rolodex/static/css/main.cb803dfc.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a13d69c5ca2cb013e61c",
+    "url": "/monsters-rolodex/static/js/main.377861ab.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
